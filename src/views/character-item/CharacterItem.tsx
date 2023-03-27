8ca8ae5ef49fb9e31b93babc2f5d36b393a93bd2
xalivo <87896066+xalivo@users.noreply.github.com>
2023-03-27 16:20:38 +0200

    useState on ChaeracterItem

--- src/views/character-item/CharacterItem.tsx
+++ src/views/character-item/CharacterItem.tsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import {Character} from "../../common/models/character.model";
-import {Star} from "@mui/icons-material";
+import {Star, StarOutline} from "@mui/icons-material";
 
 interface CharacterItemProps {
     character: Character;
 }
 
 export const CharacterItem: React.FC<CharacterItemProps> = (props) => {
-    const {character} = props;
+    const [character, setCharacter] = useState<Character>(props.character);
+
+    const handleForceSensitiveClick = () => {
+
+    }
+
     return (
         <tr>
             <td>{character.name}</td>
             <td>{character.type}</td>
             <td><img src={character.image_url} height={200}></img></td>
-            <td><Star/></td>
+            <td>{character.isForceSensitive ? <Star/> : <StarOutline/>}</td>
         </tr>
     );
 };
